dcbce9032689d32972b8a209911060d5bec52dfc
Erik Watson <evw@gloversure.co.uk>
2016-09-23 09:26:48 +0100

    The app class now conforms to js standard style

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 class App {
-  constructor() {
+  constructor () {
     console.log('This is my awesome new App!')
+  }
+
+  run () {
+    console.log('This awesome new app is running!')
   }
 }
 
 let app = new App()
+app.run()
