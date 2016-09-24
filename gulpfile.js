fd35a5f0d3226dde12eafae6ae90ff1e9e04f98e
Erik Watson <championchap@gmail.com>
2016-09-24 16:23:36 +0100

    Moving the hard coded paths out into something easier to change

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,27 @@ const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const concat = require('gulp-concat')
 
+const paths = {
+  sass_entry: './src/sass/imports.scss',
+  sass_all: './src/sass/**/*.scss',
+  css_dir: './bin/css',
+  css_file: './style.css',
+  css_maps: './maps'
+}
+
 gulp.task('default', ['sass'], () => {
 
 })
 
 gulp.task('sass', () => {
-  return gulp.src('./src/sass/imports.scss')
+  return gulp.src(paths.sass_entry)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('./style.css'))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./bin/css'))
+    .pipe(concat(paths.css_file))
+    .pipe(sourcemaps.write(paths.css_maps))
+    .pipe(gulp.dest(paths.css_dir))
 })
 
 gulp.task('watch', () => {
-  gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch(paths.sass_all, ['sass'])
 })
