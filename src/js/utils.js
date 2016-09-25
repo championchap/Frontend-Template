6c541a2c853808a9169ad729d3f6c005a542dcce
Erik Watson <championchap@gmail.com>
2016-09-25 13:48:21 +0100

    Comments

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,5 @@
 class Utils {
-  //
+  // I figure basically every app will need one of thesegulp.watch(paths.sass_all, ['sass'])
   static onPageLoaded (callback) {
     document.addEventListener('DOMContentLoaded', (event) => {
       callback()
