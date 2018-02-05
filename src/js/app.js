f5747d11566ad3fd3d42df45f9d27a829d34fce1
Erik Watson <championchap@gmail.com>
2018-02-05 21:41:14 +0000

    Renamed utils to events and got rid of all the horrible classes :)

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,15 @@
-const Utils = require('./utils.js')
+import { onPageLoaded } from './events'
 
-class App {
-  constructor () {
-    console.log('Constructed.')
-  }
+function setup () {
+  console.log('Setting up.')
+}
 
-  run () {
-    console.log('Running.')
-  }
+function run () {
+  console.log('Running.')
 }
 
 // Start the app
-Utils.onPageLoaded(() => {
-  let app = new App()
-  app.run()
+onPageLoaded(() => {
+  setup()
+  run()
 })
